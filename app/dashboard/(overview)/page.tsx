1f5ca88a9b1b9d36cadcb3e3133a7eaef1099b36
Yudi Tri Wibowo <yuditriwibowo@gmail.com>
2026-06-24 22:12:10 +0700

    force-dynamic

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,6 +6,8 @@ import { fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
 import { RevenueChartSkeleton } from "@/app/ui/skeletons";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const latestInvoices = await fetchLatestInvoices();
   const {
