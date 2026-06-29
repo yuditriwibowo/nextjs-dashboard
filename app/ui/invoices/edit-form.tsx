d87cc8598ec33ae7e2bf242d563ab86db4ccbe73
Yudi Tri Wibowo <yuditriwibowo@gmail.com>
2026-06-29 23:15:39 +0700

    -    <form action={createInvoice}> +    <form action={async (formData) => { await createInvoice(formData); }}> -    <form action={updateInvoiceWithId}> +    <form action={async (formData) => { await updateInvoiceWithId(formData); }}>

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -20,7 +20,7 @@ export default function EditInvoiceForm({
 }) {
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={async (formData) => { await updateInvoiceWithId(formData); }}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
